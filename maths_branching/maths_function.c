7cb4b04ed1f1652ad276f12d6efa0b1c5cab83ad
sabirkothiya <sabirkothiya@users.noreply.github.com>
2018-12-23 16:47:26 +0000

    Updated the maths function, so it takes two numbers and works out the remainder of the first from the second.

--- maths_branching/maths_function.c
+++ maths_branching/maths_function.c
@@ -2,23 +2,34 @@
 #include <math.h>
 
 int main() {
-    int firstNumber, secondNumber, add, subtract, multiply;
+    int firstNumber, secondNumber, add, subtract, multiply, remainder;
     float divide;
     
     // printf displays the string inside quotation
     printf("Enter two numbers\n");
     scanf("%d%d", &firstNumber, &secondNumber);
     
+    // Calculates Addition
     add = firstNumber + secondNumber;
+    
+    // Calculates Subtraction
     subtract = firstNumber - secondNumber;
+    
+    // Calculates Multiplication
     multiply = firstNumber * secondNumber;
+    
+    // Calculates Division
     divide = firstNumber / (float)secondNumber; // Typecasting
+
+    // Calculates Remainder
+    remainder = firstNumber % secondNumber;
 
     // printf displays the string inside quotation
     printf("Addition = %d\n", add);
     printf("Subtraction = %d\n", subtract);
     printf("Multiplication = %d\n", multiply);
     printf("Division = %.2f\n", divide);
+    printf("Remainder = %d\n", remainder);
     
     return 0;
 }
